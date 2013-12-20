f0102bcbe9e8410c5bdb5c37c5dab7e9f853763e
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2013-12-21 03:31:38 +0530

    basic stuff, new routes

--- example.js
+++ example.js
@@ -7,13 +7,14 @@
 var express = require('express')
   , oAuth2o = require('./oauth2o')
   , oAuth2oMongo = require('./oauth2o-mongodb')
+  , oAuth2oClient = require('./oauth2-client')
   , http = require('http')
   , crypto = require('crypto');
 
 var app = express();
 
-// var routes = new oAuth2o(oAuth2oMongo('mongodb://handw:5bttny@ds039257.mongolab.com:39257/hw_development'));
 var routes = new oAuth2o(oAuth2oMongo('mongodb://localhost/hw_development'));
+// var routes = new oAuth2o(oAuth2oMongo('mongodb://localhost/hw_development'));
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -25,11 +26,17 @@ app.use(app.router);
 
 app.post('/auth/grant', routes.createGrant);
 app.post('/auth/token', routes.createToken);
-app.post('/auth/', routes.authorize, test1);
+// app.post('/auth', routes.authorize);
+app.post('/successfulAuth', routes.authorize, successfulAuth);
+app.get('/segments-profile', routes.authorize, segmentsProfile);
 
-function test1 (req, res, next) {
-	res.json(' From Test1 ');
-}
+function successfulAuth (req, res, next) {
+  res.json(' From Test2 ');
+};
+
+function segmentsProfile (req, res, next) {
+  res.json(' From Test2 ');
+};
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
